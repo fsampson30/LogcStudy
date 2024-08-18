5c2440279756f1c4ce45b95d6b0f2ff5393e53ff
fsampson30 <fsampson30@gmail.com>
2024-08-18 13:39:25 -0300

    Implemented a switch scenario.

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,14 +1,26 @@
+import java.io.PrintStream;
+
 public class WhileLoop {
 
-    public void repetition(){
+    public void repetition() {
         boolean terminate = false;
         int times = 0;
-        while (!terminate){
+        while (!terminate) {
+            switch (times % 2) {
+                case 1: {
+                    System.out.println("odd");
+                    break;
+                }
+                case 0: {
+                    System.out.println("even");
+                    break;
+                }
+            }
             System.out.println("on the loop: " + times);
-            if (times == 10){
+            if (times == 10) {
                 terminate = true;
             }
-            times ++;
+            times++;
         }
     }
 }
